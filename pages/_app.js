aa29101066f2bdfc253ccc26551d41e4c6c3ac0a
Jonas Bjøralt <jbjora@equinor.com>
2022-08-01 12:52:40 +0200

    fix: update next-version

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, react/prop-types
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
