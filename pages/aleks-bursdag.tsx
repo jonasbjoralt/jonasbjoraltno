22fd752dbd6de5d63c24273815d933881f50f8db
Jonas Bjøralt <jbb@variant.no>
2025-04-29 12:45:32 +0200

    bd attempt 1

---
+++ pages/aleks-bursdag.tsx
@@ -0,0 +1,18 @@
+export default function GasMixer(): JSX.Element {
+  return(
+    <>
+      <Head>
+        <title>ÙwÚ Grattis! x'D </title>
+        <link rel="icon" href="whale.png" />
+      </Head>
+
+      <Page>
+        <h1>
+          Gratulerer med dagen! 
+        </h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/6n3pFFPSlW4?si=qml0HRWKj8rayQbj?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe src="https://archive.org/embed/gnomed" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen autoplay></iframe>
+      </Page>
+    </>
+  )
+}
